846fd8b3e0826a966efd1fb120e490f30218a0ef
Ivan Fesenko <pharmivan@gmail.com>
2021-02-13 20:40:45 +0200

    contacts api added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const PORT = 8080;
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("combined"));
+
+app.get("/contacts", (req, res) => {
+  res.send("data");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
