26929ec1ec9be31932d49003e4da755ea79c8976
Ivan Fesenko <pharmivan@gmail.com>
2021-02-19 11:00:10 +0200

    helpres/js deleted

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const contactRouter = require("./routes/contacts-routes.js");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
 app.use(morgan("combined"));
 app.use("/api/contacts", contactRouter);
 
